b74f0c16130ea6aef3321f9ab375cdb7b2274ebf
lin.liu <2686221966@qq.com>
2023-05-07 20:14:52 +0800

    coin-10032 解决admin-service服务登录接口访问403问题

--- coin-common/src/main/java/com/uin/config/ResourceServerConfig.java
+++ coin-common/src/main/java/com/uin/config/ResourceServerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
@@ -19,12 +20,13 @@ import org.springframework.util.FileCopyUtils;
  */
 @Configuration
 @Slf4j
+@EnableResourceServer
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
   /**
-   * ResourceServerConfigurerAdapter优先级高于ResourceServerConfigurer，用于保护oauth相关的endpoints.
-   * 同时主要作用于用户的登录（form login，Basic auth）
+   * ResourceServerConfigurerAdapter优先级高于ResourceServerConfigurer，用于保护oauth相关的endpoints. 同时主要作用于用户的登录（form login，Basic
+   * auth）
    */
   @Override
   public void configure(HttpSecurity http) throws Exception {
@@ -33,19 +35,21 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     // 基于token，不需要session
     http.sessionManagement().disable();
     http.authorizeRequests()
-        .antMatchers("/v2/api-docs",
-        "/swagger-resources/configuration/ui",// 用来获取支持的动作
-        "/swagger-resources",// 用来获取api-docs的URI
-        "/swagger-resources/configuration/security",// 安全选项
-        "/webjars/**",
-        "/swagger-ui.html"
-    ).permitAll()
+        .antMatchers(
+            "/login",
+            "/v2/api-docs",
+            "/swagger-resources/configuration/ui",// 用来获取支持的动作
+            "/swagger-resources",// 用来获取api-docs的URI
+            "/swagger-resources/configuration/security",// 安全选项
+            "/webjars/**",
+            "/swagger-ui.html"
+        ).permitAll()
         .antMatchers("/**").authenticated()
         .and().headers().cacheControl();
   }
 
   @Override
-  public void configure(ResourceServerSecurityConfigurer resources){
+  public void configure(ResourceServerSecurityConfigurer resources) {
     resources.tokenStore(jwtTokenStore());
   }
 
@@ -57,11 +61,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
   private JwtAccessTokenConverter jwtAccessTokenConverter() {
     JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
     ClassPathResource classPathResource = new ClassPathResource("jks.txt");
-    String publickey=null;
-    try{
+    String publickey = null;
+    try {
       byte[] bytes = FileCopyUtils.copyToByteArray(classPathResource.getInputStream());
-      publickey=new String(bytes,"utf-8");
-    }catch (Exception e){
+      publickey = new String(bytes, "utf-8");
+    } catch (Exception e) {
       log.info("读取公钥失败！");
     }
     jwtAccessTokenConverter.setVerifierKey(publickey);
